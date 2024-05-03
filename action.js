ec87ab80cb238e888e299af49cb6bb5408630897
Will park <ssangkist@gmail.com>
2024-05-02 23:24:37 -0700

    Adding hits of visitors

--- action.js
+++ action.js
@@ -1,13 +1,3 @@
 function greet() {
     alert('Under construction!');
 }
-function updateClock() {
-    var now = new Date();
-    var hours = now.getHours();
-    var minutes = now.getMinutes();
-    var seconds = now.getSeconds();
-    document.getElementById('clock').textContent = 
-      (hours < 10 ? '0' : '') + hours + ':' +
-      (minutes < 10 ? '0' : '') + minutes + ':' +
-      (seconds < 10 ? '0' : '') + seconds;
-}
