2a43780480593da801feeeda84156b0319ce9fed
Will park <ssangkist@gmail.com>
2024-05-05 19:30:51 -0700

    Adding js

---
+++ edu/javascript/document.js
@@ -0,0 +1,18 @@
+function fontmod() { 
+    //function to make the text bold using DOM method 
+    document.getElementById("textarea1").style.fontWeight = "bold"; 
+	//document.getElementById("textarea1").style.fontWeight = "900";
+	document.getElementById("textarea1").style.fontSizeAdjust = "0.98";
+	// document.getElementById("textarea1").style.fontStyle = "italic"; 
+	// document.getElementById("textarea1").style.textAlign = "left";
+	// document.getElementById("textarea1").style.textAlign = "center";
+	// document.getElementById("textarea1").style.textAlign = "right";
+	// document.getElementById("textarea1").style.textTransform = "uppercase";
+	// document.getElementById("textarea1").style.textTransform = "lowercase";
+	// document.getElementById("textarea1").style.textTransform = "capitalize"; 
+	// document.getElementById("textarea1").style.fontWeight = "normal"; 
+    // document.getElementById("textarea1").style.textAlign = "left"; 
+    // document.getElementById("textarea1").style.fontStyle = "normal"; 
+    document.getElementById("textarea1").style.textTransform = "capitalize"; 
+    // document.getElementById("textarea1").value = " ";
+} 
